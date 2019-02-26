d2c4482972c7e7b9d937e4b44faaaed11dea07b1
Jeff Weston <jjweston@gmail.com>
2019-02-25 21:55:23 -0800

    Increase max zoom to 20.

--- www/innert-jitters.js
+++ www/innert-jitters.js
@@ -48,7 +48,7 @@ function init()
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, " +
                      "<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " +
                      "Imagery &copy; <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
+        maxZoom: 20,
         id: "mapbox.streets",
         accessToken: accessToken
     };
